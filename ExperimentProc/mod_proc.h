395214646ba7c40993f042db7e7ad1f4663c7bd5
Vecnik88 <Ve_Cni_K@inbox.ru>
2017-07-07 01:34:47 +0300

    my experiment for proc

---
+++ ExperimentProc/mod_proc.h
@@ -0,0 +1,16 @@
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/init.h>
+#include <linux/proc_fs.h>
+#include <linux/stat.h>
+#include <asm/uaccess.h>
+#include "common.h"
+
+MODULE_LICENSE( "GPL" );
+MODULE_AUTHOR( "Vecnik88" );
+
+static int __init proc_init( void );
+static void __exit proc_exit( void );
+
+module_init( proc_init );
+module_exit( proc_exit );
