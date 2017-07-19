1ae157dcf86df3ec499d89ea42f4557d00718b2b
Vecnik88 <Ve_Cni_K@inbox.ru>
2017-07-19 22:57:48 +0300

    sys

--- Secondary/SystemCall/CR0.c
+++ Secondary/SystemCall/CR0.c
@@ -1,17 +1,17 @@
 // показать управляющий регистр CR0
-#define show_cr0() 							\
+#define show_cr0() 							          \
 { register unsigned r_eax asm ( "eax" ); 	\
-asm( "pushl %eax" ); 						\
-asm( "movl %cr0, %eax" ); 					\
-printk( "! CR0 = %x\n", r_eax ); 			\
-asm( "popl %eax"); 							\
+asm( "pushl %eax" ); 						          \
+asm( "movl %cr0, %eax" ); 					      \
+printk( "! CR0 = %x\n", r_eax ); 		    	\
+asm( "popl %eax"); 							          \
 }
 
 //код выключения защиты записи:
 #define rw_enable() 						\
 asm( "pushl %eax \n" 						\
 "movl %cr0, %eax \n" 						\
-"andl $0xfffeffff, %eax \n" 				\
+"andl $0xfffeffff, %eax \n" 		\
 "movl %eax, %cr0 \n" 						\
 "popl %eax" ); 
 
@@ -19,6 +19,6 @@ asm( "pushl %eax \n" 						\
 #define rw_disable() 						\
 asm( "pushl %eax \n" 						\
 "movl %cr0, %eax \n" 						\ 
-"orl $0x00010000, %eax \n" 					\
+"orl $0x00010000, %eax \n" 			\
 "movl %eax, %cr0 \n" 						\
 "popl %eax" );
