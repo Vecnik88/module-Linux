6a770c195e909297528324ed08b7f9fba7197fc2
Vecnik <Ve_cni_k@inbox.ru>
2017-08-13 00:15:02 +0700

    net device

--- MyWorkModule/ModuleNetwork/ModulePerCurNetwork/test_proto.c
+++ MyWorkModule/ModuleNetwork/ModulePerCurNetwork/test_proto.c
@@ -1,9 +1,119 @@
 		/* 				
-                                    	Тестовый модуль для отслеживания
-			производительности ядра с доступом через счетчики per cpu и блокироваинем 
+                                    Тестовый модуль для отслеживания
+			производительности ядра с доступом через счетчики per cpu и блокированием 
 		 */
 
-#include <net/arp.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netdevice.h>
+#include <linux/moduleparam.h>
+
+/* макросы для удобства */
+#define ERR(...) printk( KERN_ERR __VA_ARGS__ )
+#define LOG(...) printk( KERN_INFO __VA_ARGS__ )
+
+static char* virt_dev = "test%d";
+module_param( virt_dev, charp, 0 );
+
+struct net_device* netdev = NULL;
+
+int virt_rcv( struct sk_buff *skb, struct net_device *dev,
+                   struct packet_type *pt, struct net_device *odev ) {
+   LOG( "Size packet: %d\n", skb->len );
+   kfree_skb( skb );
+
+   return skb->len;
+};
+
+static struct packet_type virt_proto = {
+   __constant_htons( ETH_P_ALL ),
+   NULL,
+   virt_rcv,
+   ( void* )1,
+   NULL
+};
+static int virt_open( struct net_device* dev ) {
+	LOG( " ===== Device with module: %s up\n", THIS_MODULE->name );
+
+	netif_start_queue( dev );
+	return 0;
+}
+
+static int virt_stop( struct net_device* dev ) {
+	LOG( " ===== Device with module: %s down\n", THIS_MODULE->name );
+
+	netif_stop_queue( dev );
+	return 0;
+}
+
+static int virt_start_xmit( struct sk_buff* skb, struct net_device* dev ) {
+
+	return 0;
+}
+
+static struct net_device_ops virt_dev_ops = {
+	.ndo_open = virt_open,
+	.ndo_stop = virt_stop,
+	.ndo_start_xmit = virt_start_xmit,
+};
+
+void setup( struct net_device* dev ) {
+	int i = 0;
+	ether_setup( dev );
+	dev->netdev_ops = &virt_dev_ops;
+	for( i = 0; i < ETH_ALEN; ++i ) {
+		dev->dev_addr[ i ] = ( char )i;
+		dev->broadcast[ i ] = ( char )i;
+	}
+}
+
+static int __init virt_init( void ) {
+	int error = 0;
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 17, 0))
+	netdev = alloc_netdev( 0, virt_dev, setup );
+#else
+	netdev = alloc_netdev( 0, virt_dev, NET_NAME_UNKNOWN, setup );
+#endif
+	if( netdev == NULL ) {
+		ERR( "%s: error alloc_netdev\n", THIS_MODULE->name );
+		return -ENOMEM;
+	}
+
+	error = dev_alloc_name( netdev, netdev->name );
+	if( error != 0 ) {
+		ERR( "%s: error dev_alloc_name, error = %d\n", THIS_MODULE->name, error );
+		error = -EIO;
+		goto err;
+	}
+	register_netdev( netdev );
+
+	virt_proto.dev = netdev;
+	dev_add_pack( &virt_proto );
+
+	LOG( "===== Module %s loaded =====\n", THIS_MODULE->name );
+
+	return 0;
+
+err:
+	free_netdev( netdev );
+	return error;
+}
+
+static void __exit virt_exit( void ) {
+	dev_remove_pack( &virt_proto );
+	unregister_netdev( netdev );
+	free_netdev( netdev );
+
+	LOG( "===== Module %s unloaded =====\n", THIS_MODULE->name );
+}
+
+module_init( virt_init );
+module_exit( virt_exit );
+
+MODULE_LICENSE( "GPLv2" );
+MODULE_AUTHOR( "Vecnik88 mail:Ve_cni_K@inbox.ru" );
+
+/*#include <net/arp.h>
 #include <linux/ip.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -13,18 +123,18 @@
 #include <linux/inetdevice.h>
 #include <linux/moduleparam.h>
 
-/* макросы для удобства */
+
 #define ERR(...) printk( KERN_ERR __VA_ARGS__ )
 #define LOG(...) printk( KERN_INFO __VA_ARGS__ )
 #define DEBUG(...) if( debug > 0 ) printk( KERN_INFO "DBG MSG " __VA_ARGS__ )
 
-static char* link = "enps2n0";							/* имя родительского интерфейса */
+static char* link = "enps2n0";							8
 module_param( link, charp, 0 );
 
-static char* ifname = "interface_name_this";			/* имя виртуального интерфейса, создаваемого модулем */
+static char* ifname = "interface_name_this";			8
 module_param( ifname, charp, 0 );
 
-static int debug = 0;									/* для отладки модулей */
+static int debug = 0;									8
 module_param( debug, int, 0 );
 
 struct net_device* child = NULL;
@@ -41,7 +151,7 @@ static int test_open( struct net_device* dev ) {
 	struct in_device* in_dev = dev->ip_ptr;
 	struct in_ifaddr* ifa = in_dev->ifa_list;
 
-	netif_start_queue( dev ); 							/* Разрешаем обработку пакетов */
+	netif_start_queue( dev ); 							
 
 	return 0;
 }
@@ -64,13 +174,13 @@ static netdev_tx_t test_start_xmit( struct sk_buff* skb, struct net_device* dev 
 	/* 
 		Перед очередью этого пакета в netif_rx (),
 			Убедитесь, что dst refcounted.
-	 */
+	
 
 	skb_dst_force(skb);
 
 	skb->protocol = eth_type_trans(skb, dev);
 
-	/* нормально использовать per_cpu_ptr( ), потому что BHs отключены */
+	/* нормально использовать per_cpu_ptr( ), потому что BHs отключены 
 
 	lb_stats = this_cpu_ptr(dev->lstats);
 
@@ -114,7 +224,7 @@ static struct net_device_ops network_function = {
 	.ndo_start_xmit = test_start_xmit
 };
 
-int all_packet( struct sk_buff* skb, struct net_device* dev,			/* обработчик всех пакетов */
+int all_packet( struct sk_buff* skb, struct net_device* dev,			/* обработчик всех пакетов 
 				struct packet_type* pkt, struct net_device* odev ) {
 
 	LOG( "Function all_packet recv_packet= %d\n", skb->len );
@@ -170,3 +280,4 @@ MODULE_LICENSE( "GPL" );
 MODULE_AUTHOR( "Vecnik88" );
 MODULE_DESCRIPTION( " Тестовый модуль для изучения переменных процессора (per cpu),"
 					" чтобы в дальнейшем их модифицировать для улучшения производительности процессора" );
+*/
