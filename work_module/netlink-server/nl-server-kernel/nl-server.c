3abcac081d7217daf5a12376f7a0b6feeff626c3
Vecnik88 <Ve_Cni_K@inbox.ru>
2018-02-28 16:01:39 +0700

    refactoring

--- work_module/netlink-server/nl-server-kernel/nl-server.c
+++ work_module/netlink-server/nl-server-kernel/nl-server.c
@@ -26,22 +26,22 @@
 	       __func__, ##__VA_ARGS__)
 
 #define NL_KSERV_DBG(fmt, ...)  	       \
-	if (unlikely(nl_kserver_debug))        \
+	if (unlikely(nl_kserv_debug))          \
 	    printk(KERN_ALERT MODULE_NAME ": " \
 	           "[%s] " fmt,                \
 	           __func__, ##__VA_ARGS__)
 
 extern struct net init_net;
 static struct sock *nl_socket = NULL;
-static unsigned long nl_kserver_error = 0;
-static unsigned long nl_kserver_norm_msg = 0;
+static unsigned long nl_kserv_error = 0;
+static unsigned long nl_kserv_norm_msg = 0;
 
-int nl_kserver_debug = 0;
-module_param_named(debug_is_on, nl_kserver_debug, int, 0644);
-MODULE_PARM_DESC(debug_is_on, "Debuging this module: set in /you_path/generate_modules.sh"
-                              "(0=off debug, 1=on debug)");
+int nl_kserv_debug = 0;
+module_param_named(debug, nl_kserv_debug, int, 0644);
+MODULE_PARM_DESC(debug, "Debuging this module: set in /you_path/generate_modules.sh"
+                        "(0=off debug, 1=on debug)");
 
-static struct net *get_pointer_by_vrf(char *vrf_name)
+static struct net *get_ptr_by_vrf(char *vrf_name)
 {
 	struct net *net = NULL;
 	struct file *file = NULL;
@@ -70,24 +70,13 @@ static struct net *get_pointer_by_vrf(char *vrf_name)
 	return net;
 }
 
-static int set_carrier_vlan(struct nlmsghdr *nl_hdr)
+static int set_carrier_netdev(char *dev_name, char *vrf_name, uint16_t is_up)
 {
 	struct net *net = NULL;
-	char vlan_name[IFNAMSIZ];
-	struct net_device *vlan_dev = NULL;
-	struct vlan_carrier *vlan_opt = NULL;
-	memset(vlan_name, 0, IFNAMSIZ);
+	struct net_device *net_dev = NULL;
 
-	vlan_opt = (struct vlan_carrier *)(nlmsg_data(nl_hdr) + sizeof(struct request_to_kernel));
-	if (vlan_opt == NULL) {
-		NL_KSERV_ERR("pointer vlan_opt is null!\n");
-		return NETLINK_SERV_ERR;
-	} else if (vlan_opt->vid < 1 || vlan_opt->vid > 4094) {
-		NL_KSERV_ERR("failed number vlan!\n");
-		return NETLINK_SERV_ERR;
-	}
-	if (vlan_opt->vrf_name[0]) {
-		net = get_pointer_by_vrf(vlan_opt->vrf_name);
+	if (vrf_name[0] != '\0') {
+		net = get_ptr_by_vrf(vrf_name);
 		if (net == NULL) {
 			NL_KSERV_ERR("failed vrf name!\n");
 			return NETLINK_SERV_ERR;
@@ -95,36 +84,35 @@ static int set_carrier_vlan(struct nlmsghdr *nl_hdr)
 	} else {
 		net = &init_net;
 	}
-	snprintf(vlan_name, IFNAMSIZ, "%s%d", "vlan.", vlan_opt->vid);
-	NL_KSERV_DBG("vlan name=%s, is_up=%d, vrf name=%s!\n",
-		     vlan_name, vlan_opt->is_up,
-	       	     vlan_opt->vrf_name[0] ? vlan_opt->vrf_name : "global");
-
-	vlan_dev = __dev_get_by_name(net, vlan_name);
-	if (vlan_dev) {
-		if (vlan_opt->is_up) {
-			NL_KSERV_DBG("vlan name=%s in vrf=%s IS UP!\n",
-				      vlan_name,
-				      vlan_opt->vrf_name[0] ? vlan_opt->vrf_name : "global");
-			netif_carrier_on(vlan_dev);
+	net_dev = __dev_get_by_name(net, dev_name);
+	if (net_dev) {
+		if (is_up) {
+			NL_KSERV_DBG("net device name=%s in vrf=%s IS UP!\n",
+			             dev_name,
+			             vrf_name[0] ? vrf_name : "global");
+			netif_carrier_on(net_dev);
 		} else {
-			NL_KSERV_DBG("vlan name=%s in vrf=%s IS DOWN!\n",
-				     vlan_name,
-			             vlan_opt->vrf_name[0] ? vlan_opt->vrf_name : "global");
-			netif_carrier_off(vlan_dev);
+			NL_KSERV_DBG("net device name=%s in vrf=%s IS DOWN!\n",
+			             dev_name,
+			             vrf_name[0] ? vrf_name : "global");
+			netif_carrier_off(net_dev);
 		}
 		return 0;
 	} else {
 		for_each_net (net) {
-			vlan_dev = __dev_get_by_name(net, vlan_name);
-			if (vlan_dev) {
-				++nl_kserver_error;
-				if (vlan_opt->is_up) {
-					NL_KSERV_DBG("vlan name=%s is not find vrf IS UP!\n", vlan_name);
-					netif_carrier_on(vlan_dev);
+			net_dev = __dev_get_by_name(net, dev_name);
+			if (net_dev) {
+				++nl_kserv_error;
+				if (is_up) {
+					NL_KSERV_DBG("net device name=%s is not find vrf=%s IS UP!\n", 
+					             dev_name,
+					             vrf_name[0] ? vrf_name : "global");
+					netif_carrier_on(net_dev);
 				} else {
-					NL_KSERV_DBG("vlan name=%s is not find vrf IS DOWN!\n", vlan_name);
-					netif_carrier_off(vlan_dev);
+					NL_KSERV_DBG("net device name=%s is not find vrf=%s IS DOWN!\n",
+					             dev_name,
+					             vrf_name[0] ? vrf_name : "global");
+					netif_carrier_off(net_dev);
 				}
 				return 0;
 			}
@@ -135,9 +123,94 @@ static int set_carrier_vlan(struct nlmsghdr *nl_hdr)
 	return NETLINK_SERV_ERR;
 }
 
-static void nl_kserver_recv_msg(struct sk_buff *skb)
+static int set_carrier_vlan(struct nlmsghdr *nl_hdr)
 {
-	int pid = 0;
+	ssize_t size = 0;
+	char vlan_name[IFNAMSIZ];
+	struct vlan_carrier *vlan_opt = NULL;
+	memset(vlan_name, 0, IFNAMSIZ);
+
+	size = sizeof(struct request_to_kernel);
+	vlan_opt = (struct vlan_carrier *)(nlmsg_data(nl_hdr) + size);
+	if (vlan_opt == NULL) {
+		NL_KSERV_ERR("pointer vlan_opt is null!\n");
+		return NETLINK_SERV_ERR;
+	} else if (vlan_opt->vid < VLAN_FIRST || vlan_opt->vid > VLAN_LAST) {
+		NL_KSERV_ERR("failed number vlan!\n");
+		return NETLINK_SERV_ERR;
+	}
+	snprintf(vlan_name, IFNAMSIZ, "%s%d", "vlan.", vlan_opt->vid);
+	NL_KSERV_DBG("vlan name=%s, is_up=%d, vrf name=%s!\n",
+	             vlan_name, vlan_opt->is_up,
+	             vlan_opt->vrf_name[0] ? vlan_opt->vrf_name : "global");
+
+	return set_carrier_netdev(vlan_name,
+		                      vlan_opt->vrf_name,
+		                      vlan_opt->is_up);
+}
+
+int set_carrier_port_channel(struct nlmsghdr *nl_hdr)
+{
+	int err = 0;
+	ssize_t size = 0;
+	char port_channel_name[IFNAMSIZ];
+	struct port_channel_carrier *port_channel_opt = NULL;
+	memset(port_channel_name, 0, IFNAMSIZ);
+
+	size = sizeof(struct request_to_kernel);
+	port_channel_opt = (struct port_channel_carrier *)(nlmsg_data(nl_hdr) + size);
+	if (port_channel_opt == NULL) {
+		NL_KSERV_ERR("pointer port_channel_opt is null!\n");
+		return NETLINK_SERV_ERR;
+	} else if (port_channel_opt->num < PORT_CHANNEL_FIRST ||
+	           port_channel_opt->num > PORT_CHANNEL_LAST) {
+		NL_KSERV_ERR("failed number port channel!\n");
+		return NETLINK_SERV_ERR;
+	}
+	snprintf(port_channel_name, IFNAMSIZ, "%s%d", "vlan_po", port_channel_opt->num);
+	NL_KSERV_DBG("sub port channel name=%s, is_up=%d, vrf name=%s!\n",
+	             port_channel_name,
+	             port_channel_opt->is_up,
+	             port_channel_opt->vrf_name[0] ? port_channel_opt->vrf_name : "global");
+	if (err == NETLINK_SERV_ERR) {
+		NL_KSERV_ERR("is not set carrier port channel\n");
+		return err;
+	}
+	err = set_carrier_netdev(port_channel_name,
+	                         port_channel_opt->vrf_name,
+	                         port_channel_opt->is_up);
+	if (err == NETLINK_SERV_ERR) {
+		NL_KSERV_ERR("is not set carrier sub port channel %s\n", port_channel_name);
+		return err;
+	}
+	snprintf(port_channel_name, IFNAMSIZ, "%s%d%s", "po", port_channel_opt->num, "_vlan");
+	NL_KSERV_DBG("sub port channel name=%s, is_up=%d, vrf name=%s!\n",
+	             port_channel_name,
+	             port_channel_opt->is_up,
+	             port_channel_opt->vrf_name[0] ? port_channel_opt->vrf_name : "global");
+	err = set_carrier_netdev(port_channel_name,
+	                         port_channel_opt->vrf_name,
+	                         port_channel_opt->is_up);
+	if (err == NETLINK_SERV_ERR) {
+		NL_KSERV_ERR("is not set carrier sub port channel %s\n", port_channel_name);
+		return err;
+	}
+	snprintf(port_channel_name, IFNAMSIZ, "%s%d", "po", port_channel_opt->num);
+	NL_KSERV_DBG("port channel name=%s, is_up=%d, vrf name=%s!\n",
+	             port_channel_name,
+	             port_channel_opt->is_up,
+	             port_channel_opt->vrf_name[0] ? port_channel_opt->vrf_name : "global");
+	err = set_carrier_netdev(port_channel_name,
+	                         port_channel_opt->vrf_name,
+	                         port_channel_opt->is_up);
+
+	return err;
+}
+
+static void nl_kserv_recv_msg(struct sk_buff *skb)
+{
+	ssize_t size = 0;
+	int pid = 0, err = 0;
 	struct nlmsghdr *nl_hdr = NULL;
 	struct reply_with_kernel reply;
 	struct sk_buff *skb_to_user = NULL;
@@ -147,58 +220,66 @@ static void nl_kserver_recv_msg(struct sk_buff *skb)
 	nl_hdr = (struct nlmsghdr *)skb->data;
 	if (nl_hdr == NULL) {
 		NL_KSERV_ERR("failed packet!\n");
-		reply.err = NETLINK_SERV_ERR;
+		err = NETLINK_SERV_ERR;
 		goto out;
 	}
 	pid = nl_hdr->nlmsg_pid;
 	request = (struct request_to_kernel *)nlmsg_data(nl_hdr);
 	if (request == NULL) {
 		NL_KSERV_ERR("failed request with kernel!\n");
-		reply.err = NETLINK_SERV_ERR;
+		err = NETLINK_SERV_ERR;
 		goto out;
 	}
 	switch (request->cmd) {
 		case SET_VLAN_CARRIER:
-			reply.err = set_carrier_vlan(nl_hdr);
+			err = set_carrier_vlan(nl_hdr);
 			break;
+		case SET_PORT_CHANNEL_CARRIER:
+			err = set_carrier_port_channel(nl_hdr);
+			break;
+		case GET_VLAN_CARRIER:
+		case GET_PORT_CHANNEL_CARRIER:
 		default:
-			reply.err = NETLINK_SERV_ERR;
+			NL_KSERV_ERR("incorrect command to kernel!\n");
+			err = NETLINK_SERV_ERR;
 			break;
 	}
 
 out:
-	if (reply.err == NETLINK_SERV_ERR)
-		++nl_kserver_error;
+	if (err == NETLINK_SERV_ERR)
+		++nl_kserv_error;
 	else
-		++nl_kserver_norm_msg;
+		++nl_kserv_norm_msg;
 
 	skb_to_user = nlmsg_new(MAX_PAYLOAD, GFP_KERNEL);
 	if (skb_to_user == NULL) {
-		++nl_kserver_error;
+		++nl_kserv_error;
 		NL_KSERV_ERR("failed to allocate memory!\n");
 		return;
 	}
+	reply.err = err;
 	nl_hdr = nlmsg_put(skb_to_user, 0, 0, NLMSG_DONE, MAX_PAYLOAD, 0);
 	NETLINK_CB(skb_to_user).dst_group = 0;
-	memcpy((void *)nlmsg_data(nl_hdr), (void *)&reply, sizeof(struct reply_with_kernel));
+	size = sizeof(struct reply_with_kernel);
+	memcpy((void *)nlmsg_data(nl_hdr), (void *)&reply, size);
 	if (nlmsg_unicast(nl_socket, skb_to_user, pid) < 0) {
-		++nl_kserver_error;
+		++nl_kserv_error;
 		NL_KSERV_ERR("failed send message to user-space!\n");
 	}
 }
 
 struct netlink_kernel_cfg cfg = {
-	.input = nl_kserver_recv_msg,
+	.input = nl_kserv_recv_msg,
 };
 
-static ssize_t nl_kserver_read_info(struct file *file, char *buf,
+static ssize_t nl_kserv_read_info(struct file *file, char *buf,
                                     size_t count, loff_t *ppos)
 {
 	static char buf_msg[LEN_MSG];
 	memset(buf_msg, 0, LEN_MSG);
 
-	snprintf(buf_msg, LEN_MSG, "%s%lu\n%s%lu\n", "Normal message: ", nl_kserver_norm_msg,
-	                                             "Errors: ", nl_kserver_error);
+	snprintf(buf_msg, LEN_MSG, "%s%lu\n%s%lu\n", "Normal message: ", nl_kserv_norm_msg,
+	                                             "Errors: ", nl_kserv_error);
 
 	if (*ppos >= strlen(buf_msg)) {
 		*ppos = 0;
@@ -206,7 +287,6 @@ static ssize_t nl_kserver_read_info(struct file *file, char *buf,
 	}
 	if (count > strlen(buf_msg) - *ppos)
 		count = strlen(buf_msg) - *ppos;
-
 	if (copy_to_user((void *)buf, buf_msg + *ppos, count)) {
 		NL_KSERV_ERR("can't copy data to user space\n");
 		return -EFAULT;
@@ -216,19 +296,19 @@ static ssize_t nl_kserver_read_info(struct file *file, char *buf,
 	return count;
 }
 
-static ssize_t nl_kserver_control(struct file *file, const char *buf,
-                                  size_t count, loff_t *ppos)
+static ssize_t nl_kserv_control(struct file *file, const char *buf,
+                                size_t count, loff_t *ppos)
 {
 	return 0;
 }
 
 static const struct file_operations nl_fops = {
 	.owner = THIS_MODULE,
-	.read = nl_kserver_read_info,
-	.write = nl_kserver_control
+	.read = nl_kserv_read_info,
+	.write = nl_kserv_control
 };
 
-static int __init nl_kserver_init(void)
+static int __init nl_kserv_init(void)
 {
 	struct proc_dir_entry *nl_node = NULL;
 
@@ -247,7 +327,7 @@ static int __init nl_kserver_init(void)
 	return 0;
 }
 
-static void __exit nl_kserver_exit(void)
+static void __exit nl_kserv_exit(void)
 {
 	netlink_kernel_release(nl_socket);
 	remove_proc_entry(MODULE_NAME, NULL);
@@ -255,8 +335,8 @@ static void __exit nl_kserver_exit(void)
 	NL_KSERV_ERR("unloaded!\n");
 }
 
-module_init(nl_kserver_init);
-module_exit(nl_kserver_exit);
+module_init(nl_kserv_init);
+module_exit(nl_kserv_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Netlink server");
